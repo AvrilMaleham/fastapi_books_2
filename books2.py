17acaeaba7674b74f264c3fa8b8a45adb18bf4c7
Avril Maleham <avrilmaleham@outlook.com>
2025-03-09 07:34:59 +1300

    feat: get by published date endpoint

--- books2.py
+++ books2.py
@@ -10,13 +10,15 @@ class Book:
     author: str
     description: str
     rating: int
+    published_date: int
     
-    def __init__ (self, id, title, author, description, rating):
+    def __init__ (self, id, title, author, description, rating, published_date):
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.published_date = published_date
         
 class BookRequest(BaseModel):
     id: Optional[int] = Field(description="id is not needed on create", default=None)
@@ -24,6 +26,7 @@ class BookRequest(BaseModel):
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=-1, lt=6)
+    published_date: int = Field(gt=1899, lt=2026)
     
     model_config = {
         "json_schema_extra": {
@@ -31,19 +34,20 @@ class BookRequest(BaseModel):
                 "title": "A new book",
                 "author": "codingwithroby",
                 "description": "A new description of a book.",
-                "rating": 5
+                "rating": 5,
+                "published_date": 2012
             }
         }
     }
         
 
 BOOKS = [
-    Book(1, "Computer Science Pro", "codingwithroby", "A very nice book.", 5),
-    Book(2, "Be Fast with FastAPI", "codingwithroby", "A great book.", 5),
-    Book(3, "Master Endpoints", "codingwithroby", "An awesome book.", 5),
-    Book(4, "HP1", "Author 1", "Book description.", 3),
-    Book(5, "HP2", "Author 2", "Book description.", 4),
-    Book(6, "HP3", "Author 3", "Book description.", 2),
+    Book(1, "Computer Science Pro", "codingwithroby", "A very nice book.", 5, 2023),
+    Book(2, "Be Fast with FastAPI", "codingwithroby", "A great book.", 5, 2024),
+    Book(3, "Master Endpoints", "codingwithroby", "An awesome book.", 5, 2025),
+    Book(4, "HP1", "Author 1", "Book description.", 3, 1999),
+    Book(5, "HP2", "Author 2", "Book description.", 4, 1999),
+    Book(6, "HP3", "Author 3", "Book description.", 2, 1999),
     ]
 
 @app.get("/books")
@@ -64,8 +68,16 @@ async def read_book_by_rating(book_rating: int):
             books_to_return.append(book)
             
     return books_to_return
+
+@app.get("/books/published_date/")
+async def read_book_by_published_date(published_date: int):
+    books_to_return = []
+    for book in BOOKS:
+        if book.published_date == published_date:
+            books_to_return.append(book)
             
-    
+    return books_to_return
+            
 @app.post("/create-book")
 async def create_book(book_request: BookRequest):
     new_book = Book(**book_request.model_dump())
